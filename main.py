e27e2504a9f993ad973430317e929340e8251dd8
Serios7 <Serios601@gmail.com>
2017-10-25 13:46:15 +1000

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+# Вариант №3
+# 3. Дан файл. Определите сколько в нем букв (латинского алфавита), слов, строк. Выведите три найденных числа в формате, приведенном в примере.
+# Пример входного файла:
+# Beautiful is better than ugly.
+# Explicit is better than implicit.
+# Simple is better than complex.
+# Complex is better than complicated.
+# Пример выходного файла:
+# Input file contains:
+# 108 letters
+# 20 words
+# 4 lines
+
